883a97c5887f22abb0e299430aa04bf15d6faa45
haijuf30 <haikal.aljufri30tif@gmail.com>
2024-10-01 13:48:37 +0700

    sehabis download

--- proyek3/routes/web.php
+++ proyek3/routes/web.php
@@ -1,22 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controller;
+use Livewire\Volt\Volt;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
-Route::get('/', function () {
-    return view('app');
-});
-
-Route::get('/lkpd', [Controller::class, 'index'])->name('lkpd');
+Volt::route('/', 'users.index');
